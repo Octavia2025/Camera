b3b37d15465a828fb3ed8f32a2318f884d8452b3
Octavia2025 <octaviatmucheche@gmail.com>
2026-04-29 12:50:06 +0200

    Initial Commit

---
+++ Camera2.0/Camera2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Camera2_0App.swift
+//  Camera2.0
+//
+//  Created by octavia on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Camera2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camera2.0
+//
+//  Created by octavia on 29/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
